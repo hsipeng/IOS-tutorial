ad975bef6a1a193908280148585374de7a283266
lirawx <lira_wx@outlook.com>
2020-12-29 11:31:15 +0800

    feat: init demo

---
+++ projects/Objectc-Hello-world-NoStoryBoard/Objectc-Hello-world-NoStoryBoard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Objectc-Hello-world-NoStoryBoard
+//
+//  Created by 彭熙 on 2020/12/29.
+//  Copyright © 2020 彭熙@lirawx.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ projects/Objectc-Hello-world-NoStoryBoard/Objectc-Hello-world-NoStoryBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Objectc-Hello-world-NoStoryBoard
+//
+//  Created by 彭熙 on 2020/12/29.
+//  Copyright © 2020 彭熙@lirawx.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
